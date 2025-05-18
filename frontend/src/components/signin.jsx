b25d734571112a33b4e1b34060a18c67ff0038fa
khanhdiba <dinhbakhanh1508@gmail.com>
2025-05-18 17:56:22 +0700

    merge

--- frontend/src/components/signin.jsx
+++ frontend/src/components/signin.jsx
@@ -1,39 +1,120 @@
-import React from "react";
+// import React from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import "./box.css"
+
+// function SignInBox(){
+//     const navigate = useNavigate();
+
+//     return (
+//         <div className="box">
+//             <div className="title">Sign in</div>
+
+//             <div className="accountbox">
+//                 <div className="input">
+//                     <div className="text">Account</div>
+//                     <input className="inbox" type="text" placeholder="Enter your account"></input>
+//                 </div>
+//             </div>
+
+//             <div className="passwordbox">
+//                 <div className="input">
+//                     <div className="text">Password</div>
+//                     <input className="inbox" type="password" placeholder="Enter your password"></input>
+//                 </div>
+//             </div>
+
+//             <label className="rememberme">
+//               <input type="checkbox" />
+//               Remember me
+//             </label>
+
+//             <div className="donthave">
+//                 Don't have an account? <Link style={{textDecoration: "underline", cursor: "pointer"}} to="/signup">Register</Link>
+//             </div>
+
+//             <button className="button" onClick = {() => navigate("/dashboard")}>Sign in</button>
+//         </div>
+//     )
+// };
+// export default SignInBox;
+
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./box.css"
+import "./box.css";
 
-function SignInBox(){
-    const navigate = useNavigate();
+function SignInBox() {
+  const navigate = useNavigate();
+  const [account, setAccount] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
-    return (
-        <div className="box">
-            <div className="title">Sign in</div>
+  const handleSignIn = async () => {
+    const res = await fetch("/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: account, password }),
+    });
 
-            <div className="accountbox">
-                <div className="input">
-                    <div className="text">Account</div>
-                    <input className="inbox" type="text" placeholder="Enter your account"></input>
-                </div>
-            </div>
+    const data = await res.json();
+    if (res.ok) {
+      // Optional: Save user to localStorage
+      localStorage.setItem("user", JSON.stringify(data.user));
 
-            <div className="passwordbox">
-                <div className="input">
-                    <div className="text">Password</div>
-                    <input className="inbox" type="password" placeholder="Enter your password"></input>
-                </div>
-            </div>
+      navigate("/dashboard");
+    } else {
+      setErrorMsg(data.message);
+    }
+  };
 
-            <label className="rememberme">
-              <input type="checkbox" />
-              Remember me
-            </label>
+  return (
+    <div className="box">
+      <div className="title">Sign in</div>
 
-            <div className="donthave">
-                Don't have an account? <Link style={{textDecoration: "underline", cursor: "pointer"}} to="/signup">Register</Link>
-            </div>
-
-            <button className="button" onClick = {() => navigate("/dashboard")}>Sign in</button>
+      <div className="accountbox">
+        <div className="input">
+          <div className="text">Account</div>
+          <input
+            className="inbox"
+            type="text"
+            placeholder="Enter your account"
+            value={account}
+            onChange={(e) => setAccount(e.target.value)}
+          />
         </div>
-    )
-};
+      </div>
+
+      <div className="passwordbox">
+        <div className="input">
+          <div className="text">Password</div>
+          <input
+            className="inbox"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <label className="rememberme">
+        <input type="checkbox" />
+        Remember me
+      </label>
+
+      {errorMsg && <div className="error" style={{ color: "red", marginTop: "10px" }}>{errorMsg}</div>}
+
+      <div className="donthave">
+        Don't have an account?{" "}
+        <Link style={{ textDecoration: "underline", cursor: "pointer" }} to="/signup">
+          Register
+        </Link>
+      </div>
+
+      <button className="button" onClick={handleSignIn}>
+        Sign in
+      </button>
+    </div>
+  );
+}
+
 export default SignInBox;
