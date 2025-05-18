6792b9776e00647a41b5415dfd373729dfc7dd50
Thục Khuê <thuckhue04@gmail.com>
2025-05-18 17:25:32 +0700

    Khuethuc (#21)
    * update
    
    * update fridge page

--- frontend/src/pages/fridge/fridge.jsx
+++ frontend/src/pages/fridge/fridge.jsx
@@ -8,6 +8,7 @@ import Footer from "../../components/footer";
 export default function Fridge(){
     const [img, setImg] = useState(null);
     const [seeButton, setSeeButton] = useState(null);
+    const [light, SetLight] = useState(null);
 
     /* Fetch image */
 
@@ -21,12 +22,14 @@ export default function Fridge(){
 
     const timestamp = "TIMESTAMP";
 
+
     const vegetables = [
         { name: "Broccoli", quantity: 1},
         { name: "Bell Pepper", quantity: 3},
         { name: "Watermelon", quantity: 1},
         { name: "Lettuce", quantity: "Yes"}
     ];
+
 
     const dishes = [
         { name: "Dish 1"},
@@ -42,6 +45,12 @@ export default function Fridge(){
                 <div className = "image-container">
                     {img && <img src={`data:image/jpeg;base64,${img}`} alt="IoT Image" className = "fridge-image" />}
                     <p style={{fontStyle: "italic"}}>Updated at {timestamp}</p>
+                    <button
+                        onClick = {() => SetLight(!light)}
+                        className = {light ? "light-on-btn" : "light-off-btn"}
+                    >
+                        {light ? "Light: On" : "Light: Off"}
+                    </button>
                 </div>
 
                 {/* Tables */}
@@ -68,6 +77,12 @@ export default function Fridge(){
                                             >
                                                 {seeButton === index ? "Hide" : "See"}
                                             </button>
+                                            <button
+                                                className="see-btn"
+                                                onClick={() => setSeeButton(seeButton === index ? null : index)}
+                                            >
+                                                {seeButton === index ? "Hide" : "See"}
+                                            </button>
                                         </td>
                                     </tr>
                                 ))}
@@ -75,6 +90,24 @@ export default function Fridge(){
                         </table>
                     </div>
                     {/* Dishes */}
+                    {seeButton !== null && (
+                        <div className="dish-table">
+                            <table>
+                                <thead>
+                                <tr>
+                                    <th>Dish</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    {dishes.map((dish, index) => (
+                                        <tr key = {index}>
+                                            <td key = {index}>{dish.name}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
                     {seeButton !== null && (
                         <div className="dish-table">
                             <table>
